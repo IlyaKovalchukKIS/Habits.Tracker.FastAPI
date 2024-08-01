3593db7bbd8e2ab23fbf3661c51b589f43af7069
user <mrlivedance@vk.com>
2024-08-01 15:38:24 +0300

    initial project

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("src.main:app", reload=True, port=8000)
