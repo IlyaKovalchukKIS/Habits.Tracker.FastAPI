76067e460fdb1fff63a3cac5c6ad30fcb57a2993
user <mrlivedance@vk.com>
2024-08-03 00:24:49 +0300

    create config pydantic-settings

---
+++ src/repositories/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/postgres"
+
+    @property
+    def DATABASE_URL_psycopg(self):
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/postgres"
+
+    model_config = SettingsConfigDict(env_file="../../.env")
+
+
+settings = Settings()
