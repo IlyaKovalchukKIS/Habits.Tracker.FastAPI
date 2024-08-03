f84c1dec909b8f8450c6581ed5c309cc2745ecfd
user <mrlivedance@vk.com>
2024-08-03 20:24:32 +0300

    add docstrings

--- src/repositories/models/user.py
+++ src/repositories/models/user.py
@@ -4,10 +4,23 @@ from pydantic import EmailStr
 from sqlalchemy import text
 from sqlalchemy.orm import relationship, mapped_column, Mapped
 
-from src.repositories import Base, str_40
+from src.repositories import Base, str_40, HabitModels
 
 
 class UserOrm(Base):
+    """
+    id: номер пользователя
+    email: почта пользователя
+    name_telegram: имя пользователя
+    id_telegram: id пользователя в телеграм
+    hashed_password: хэш пароля
+    created_at: дата создания пользователя
+    is_active: активен ли пользователь
+    is_superuser: является ли пользователь суперпользователем
+    is_admin: является ли пользователь админом
+    habits: список привычек пользователя
+    """
+
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
