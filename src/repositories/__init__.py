cc60d74eaf90170f8fa2275031e1bf14df44ce85
user <mrlivedance@vk.com>
2024-08-03 20:12:18 +0300

    add import to init file -  Frequency

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -6,10 +6,12 @@ __all__ = [
     "DatabaseHelper",
     "Base",
     "str_40",
+    "Frequency",
 ]
 
 from crud.user import UserOrmCrud
 from crud.habit import HabitOrmCrud
 from models.user import UserOrm as UserModels
 from models.habit import HabitOrm as HabitModels
+from models.habit import Frequency
 from db_helper import DatabaseHelper, Base, str_40
