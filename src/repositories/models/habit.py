f3ee8b5b1e62013c144031cf222c023cf1a2e8e3
user <mrlivedance@vk.com>
2024-08-03 16:30:02 +0300

    refactor dir

--- src/repositories/models/habit.py
+++ src/repositories/models/habit.py
@@ -1,11 +1,11 @@
-from datetime import datetime
-from typing import Optional, Annotated
-
-from sqlalchemy import Boolean, ForeignKey, Integer, String, text
-from sqlalchemy.orm import relationship, mapped_column, Mapped, DeclarativeBase
 import enum
+from datetime import datetime
 
-str_40 = Annotated[str, mapped_column(String(40))]
+from sqlalchemy import ForeignKey, text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.repositories.db_helper import Base, str_40
+from src.repositories.models.user import UserOrm
 
 
 class Frequency(enum.Enum):
@@ -18,49 +18,45 @@ class Frequency(enum.Enum):
     DAILY_SEVEN = 7  # 1 раз в 7 дней
 
 
-class Base(DeclarativeBase):
-    pass
-
-
-class UserOrm(Base):
-    __tablename__ = "user"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(unique=True, index=True)
-    id_telegram: Mapped[int] = mapped_column(unique=True, index=True)
-    hashed_password: Mapped[str]
-    is_active: Mapped[bool] = mapped_column(default=True)
-
-    habits = relationship("HabitOrm", back_populates="user")
-
-
 class HabitOrm(Base):
     __tablename__ = "habits"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    place: Mapped[str]  # место выполнения привычки
+
+    place: Mapped[str_40]  # место выполнения привычки
+
     time: Mapped[datetime]  # время начала выполнения привычки
-    action: Mapped[str]  # действие привычки
+
+    action: Mapped[str_40]  # действие привычки
+
     pleasant_habit: Mapped[bool | None] = mapped_column(
         nullable=True, default=False
     )  # признак приятной привычки
+
     related_habit: Mapped[int | None] = mapped_column(
         ForeignKey("habits.id"), nullable=True
     )  # связанная привычка
+
     frequency: Mapped[Frequency] = mapped_column(
         default=Frequency.DAILY_ONE
     )  # частота выполнения
-    award: Mapped[str | None]  # награда
+
+    award: Mapped[str_40 | None]  # награда
+
     time_to_complete: Mapped[int | None] = mapped_column(
         default=120
     )  # время на выполнение
+
     is_published: Mapped[bool | None] = mapped_column(
         default=False
     )  # публикация привычки
+
     last_send: Mapped[datetime | None]  # время последнего выполнения привычки
+
     created_at: Mapped[datetime] = mapped_column(
         server_default=text("TIMEZONE('utc', now())")
     )  # время создания привычки
+
     user_id: Mapped[int] = mapped_column(
         ForeignKey("user.id", ondelete="CASCADE")
     )  # id пользователя
---
+++ src/repositories/models/user.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pydantic import EmailStr
+from sqlalchemy import text
+from sqlalchemy.orm import relationship, mapped_column, Mapped
+
+from src.repositories.db_helper import Base, str_40
+
+
+class UserOrm(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[EmailStr] = mapped_column(unique=True, index=True)
+    id_telegram: Mapped[int] = mapped_column(unique=True, index=True)
+    hashed_password: Mapped[str_40]
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())")
+    )
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
+    is_admin: Mapped[bool] = mapped_column(default=False)
+
+    habits = relationship("HabitOrm", back_populates="user")
