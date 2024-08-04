e703adffcec7c215c2882a55e1ca96ae005ad586
user <mrlivedance@vk.com>
2024-08-04 17:35:20 +0300

    delete import in init

--- src/config/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = [
-    "settings",
-]
-
-from .config import settings
--- src/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = [
-    "user_schemas",
-    "habit_schemas",
-]
-
-import user as user_schemas
-import habit as habit_schemas
