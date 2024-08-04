93bec32dee7982fd0333b26ee00886e994bec637
user <mrlivedance@vk.com>
2024-08-04 23:04:37 +0300

    refactor model to model from fastapi-users
    Signed-off-by: user <mrlivedance@vk.com>

--- src/repositories/models/user.py
+++ src/repositories/models/user.py
@@ -1,38 +1,37 @@
 from datetime import datetime
 
-from pydantic import EmailStr
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import text
-from sqlalchemy.orm import relationship, mapped_column, Mapped
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.repositories.db_helper import Base, str_40
+from src.repositories.db_helper import Base
 
 
-class UserOrm(Base):
+class UserOrm(SQLAlchemyBaseUserTable[int], Base):
     """
-    id: номер пользователя
-    email: почта пользователя
-    name_telegram: имя пользователя
-    id_telegram: id пользователя в телеграм
-    hashed_password: хэш пароля
-    created_at: дата создания пользователя
-    is_active: активен ли пользователь
-    is_superuser: является ли пользователь суперпользователем
-    is_admin: является ли пользователь админом
-    habits: список привычек пользователя
-    """
-
+        id: номер пользователя
+        email: почта пользователя
+        name_telegram: имя пользователя
+        id_telegram: id пользователя в телеграм
+        hashed_password: хэш пароля
+        created_at: дата создания пользователя
+        is_active: активен ли пользователь
+        is_superuser: является ли пользователь суперпользователем
+        is_verified: подтвержден ли пользователь
+        habits: список привычек пользователя
+        """
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True, index=True)
     name_telegram: Mapped[str]
     id_telegram: Mapped[int] = mapped_column(unique=True, index=True, nullable=True)
-    hashed_password: Mapped[str_40]
+    hashed_password: str
     created_at: Mapped[datetime] = mapped_column(
         server_default=text("TIMEZONE('utc', now())")
     )
-    is_active: Mapped[bool] = mapped_column(default=True)
-    is_superuser: Mapped[bool] = mapped_column(default=False)
-    is_admin: Mapped[bool] = mapped_column(default=False)
+    is_active: bool
+    is_superuser: bool
+    is_verified: bool
 
     habits = relationship("HabitOrm", back_populates="user")
