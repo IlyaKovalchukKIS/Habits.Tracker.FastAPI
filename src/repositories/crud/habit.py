d04f1e29a8eebbf3a83449a9f2d664424e54d548
user <mrlivedance@vk.com>
2024-08-07 01:18:19 +0300

    add validation and create habits
    Signed-off-by: user <mrlivedance@vk.com>

--- src/repositories/crud/habit.py
+++ src/repositories/crud/habit.py
@@ -1,22 +1,35 @@
+from sqlalchemy import select, Result
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.repositories.models.habit import HabitOrm
+from src.schemas import habit as schemas
+from src.schemas.habit import HabitRead
 
 
 class HabitOrmCrud:
 
     @staticmethod
-    async def insert_habit(session: AsyncSession, data: dict):
+    async def insert_habit(session: AsyncSession, data: schemas.HabitCreate, user):
         """Создание привычки"""
-        pass
+        data = data.model_dump()
+        data["user_id"] = user.id  # user.id
+        habit = HabitOrm(**data)
+        session.add(habit)
+        await session.commit()
+        await session.refresh(habit)
+        return habit
 
     @staticmethod
     async def get_habit_by_id(session: AsyncSession, habit_id: int):
         """Получение привычки по id"""
-        pass
+        return await session.get(HabitOrm, habit_id)
 
     @staticmethod
     async def get_habit_by_user_id(session: AsyncSession, user_id: int):
         """Получение привычки по id пользователя"""
-        pass
+        return await session.execute(
+            select(HabitOrm).where(HabitOrm.user_id == user_id)
+        )
 
     @staticmethod
     async def get_all_habits(session: AsyncSession):
--- src/routers/habit.py
+++ src/routers/habit.py
@@ -2,21 +2,40 @@ from typing import List
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.responses import JSONResponse
 
+from src.auth.database import fastapi_users
+from src.repositories.crud.habit import HabitOrmCrud
 from src.repositories.db_helper import db_helper
+from src.repositories.models.user import UserOrm
 from src.schemas import habit as habit_schemas
+from src.validators.habit import HabitValidator
 
 habit_routers = APIRouter(prefix="/habit", tags=["Habit"])
 
 current_user = fastapi_users.current_user()
 
 
-@habit_routers.post("/create/", response_model=habit_schemas.HabitCreate)
+@habit_routers.post("/create/", response_model=habit_schemas.HabitRead)
 async def create_habit(
-    data: dict, session: AsyncSession = Depends(db_helper.session_dependency)
+    data: habit_schemas.HabitCreate = Depends(habit_schemas.HabitCreate),
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    user: UserOrm = Depends(current_user),
 ):
     """Создание привычки"""
-    pass
+    related_habit = data.model_dump().get("related_habit", None)
+
+    if related_habit:
+        get_related_habit = await HabitOrmCrud.get_habit_by_id(session, related_habit)
+        get_related_habit = get_related_habit.pleasant_habit
+    else:
+        get_related_habit = None
+    validation = HabitValidator("related_habit", "award", "pleasant_habit").__call__(
+        data.model_dump(), get_related_habit
+    )
+    if validation:
+        return JSONResponse(status_code=400, content={"error": validation})
+    return await HabitOrmCrud.insert_habit(session, data, user)
 
 
 @habit_routers.get("/get/by/{habit_id}", response_model=habit_schemas.HabitRead)
