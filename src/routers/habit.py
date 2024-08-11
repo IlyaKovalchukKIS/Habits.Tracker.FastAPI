b3579394e65f5fb424989d0286b53707126777ef
user <mrlivedance@vk.com>
2024-08-11 23:40:23 +0300

    change routers
    Signed-off-by: user <mrlivedance@vk.com>

--- src/routers/habit.py
+++ src/routers/habit.py
@@ -1,5 +1,6 @@
 from typing import List, Annotated
 
+from attr import attrs
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.responses import JSONResponse
@@ -7,6 +8,7 @@ from starlette.responses import JSONResponse
 from src.auth.database import fastapi_users
 from src.repositories.crud.habit import HabitOrmCrud
 from src.repositories.db_helper import db_helper
+from src.repositories.models.habit import HabitOrm
 from src.repositories.models.user import UserOrm
 from src.schemas import habit as habit_schemas
 from src.validators.habit import HabitValidator
@@ -16,57 +18,51 @@ habit_routers = APIRouter(prefix="/habit", tags=["Habit"])
 current_user = fastapi_users.current_user()
 
 
-@habit_routers.post("/create/", response_model=habit_schemas.HabitRead)
+@habit_routers.post("/create/", response_model=habit_schemas.HabitRead | dict)
 async def create_habit(
     data: habit_schemas.HabitCreate = Depends(habit_schemas.HabitCreate),
     session: AsyncSession = Depends(db_helper.session_dependency),
     user: UserOrm = Depends(current_user),
 ):
     """Создание привычки"""
-
-    related_habit_id = data.model_dump().get("related_habit")
-    related_habit = (
-        await HabitOrmCrud.get_habit_by_id(session, related_habit_id)
-        if related_habit_id
-        else False
-    )
-    print(related_habit)
-    pleasant_habit = related_habit.pleasant_habit if related_habit else None
-    print(pleasant_habit)
-
-    validation = HabitValidator("related_habit", "award", "pleasant_habit")(
-        data.model_dump(), pleasant_habit
-    )
-
-    if validation:
-        return JSONResponse(status_code=400, content={"error": validation})
-
-    return await HabitOrmCrud.insert_habit(session, data, user)
+    try:
+        related_habit = None
+        if data.related_habit:
+            related_habit = await HabitOrmCrud.get_habit_by_id(
+                session, data.related_habit, user.id
+            )
+            if related_habit is None:
+                raise ValueError("Привычка не найдена")
+        await HabitValidator()(data, related_habit, user.id)
+    except ValueError as e:
+        return {"ValidationError": str(e)}
+    return await HabitOrmCrud.insert_habit(session, data, user.id)
 
 
-@habit_routers.get("/get/by/{habit_id}", response_model=habit_schemas.HabitRead)
+@habit_routers.get("/get/{habit_id}", response_model=habit_schemas.HabitRead)
 async def get_habit_by_id(
-    habit_id: int, session: AsyncSession = Depends(db_helper.session_dependency)
+    habit_id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    user: UserOrm = Depends(current_user),
 ):
     """Получение привычки по id"""
-    return await HabitOrmCrud.get_habit_by_id(session, habit_id)
+    return await HabitOrmCrud.get_habit_by_id(session, habit_id, user.id)
 
 
-@habit_routers.get("/get/by/user/", response_model=List[habit_schemas.HabitRead])
+@habit_routers.get("/get/all/", response_model=List[habit_schemas.HabitRead])
 async def get_habit_by_user_id(
     user: UserOrm = Depends(current_user),
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
     """Получение привычки по id пользователя"""
-    return await HabitOrmCrud.get_habit_by_user_id(session, user.id)
-
-
-# @habit_routers.get("/get/all/", response_model=List[habit_schemas.HabitRead])
-# async def get_all_habits(
-#     session: AsyncSession = Depends(db_helper.session_dependency),
-# ):
-#     """Получение всех привычек"""
-#     pass
+    get_all_habit = await HabitOrmCrud.get_habit_by_user_id(session, user.id)
+    for habit in get_all_habit:
+        if habit.related_habit:
+            relation_habit = await HabitOrmCrud.get_habit_by_id(
+                session, habit.related_habit, user.id
+            )
+            habit.related_habit = [relation_habit]
+    return get_all_habit
 
 
 @habit_routers.get("/get/published/", response_model=List[habit_schemas.HabitRead])
@@ -74,10 +70,11 @@ async def get_published_habits(
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
     """Получение публичных привычек"""
-    return await HabitOrmCrud.get_published_habits(session)
+    result = await HabitOrmCrud.get_published_habits(session)
+    return result
 
 
-@habit_routers.put("/update/", response_model=habit_schemas.HabitUpdate)
+@habit_routers.patch("/update/", response_model=habit_schemas.HabitRead | dict)
 async def update_habit(
     habit_update: Annotated[habit_schemas.HabitCreate, Depends()],
     habit_id: int,
@@ -85,14 +82,22 @@ async def update_habit(
     user: UserOrm = Depends(current_user),
 ):
     """Обновление привычки"""
-    habit = await HabitOrmCrud.get_habit_by_id(session, habit_id)
-    result = await HabitOrmCrud.update_habit(session, habit, habit_update.model_dump())
-    return result
+    # habit = await HabitOrmCrud.get_habit_by_id(session, habit_id, user.id)
+    # validation = await HabitValidator(
+    #     related_habit="related_habit", award="award", pleasant_habit="pleasant_habit"
+    # )(data=habit_update.model_dump(), user=user, session=session)
+    # if validation:
+    #     return {"error": validation}
+    # print("return")
+    # return await HabitOrmCrud.update_habit(session, habit, habit_update.__dict__)
+    pass
 
 
 @habit_routers.delete("/delete/")
 async def delete_habit(
-    habit_id: int, session: AsyncSession = Depends(db_helper.session_dependency)
+    habit_id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+    user: UserOrm = Depends(current_user),
 ):
     """Удаление привычки"""
-    pass
+    return await HabitOrmCrud.delete_habit(session, habit_id, user.id)
