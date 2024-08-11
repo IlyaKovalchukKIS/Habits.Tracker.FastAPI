5fb7ca7b72622b01ad071138b3d0d44c0d68c269
user <mrlivedance@vk.com>
2024-08-11 23:39:42 +0300

    change validator class
    Signed-off-by: user <mrlivedance@vk.com>

--- src/validators/habit.py
+++ src/validators/habit.py
@@ -1,60 +1,35 @@
+from src.repositories.models.habit import HabitOrm
+from src.schemas.habit import HabitCreate
+
+
 class HabitValidator:
-    """Валидатор привычки"""
 
-    def __init__(self, related_habit: str, award: str, pleasant_habit: str):
-        self.related_habit = related_habit
-        self.award = award
-        self.pleasant_habit = pleasant_habit
-
-    def __call__(self, value: dict, related_habit_instance: dict | None = None):
-        related_habit = dict(value).get(self.related_habit)  # связанная привычка
-        pleasant_habit = dict(value).get(
-            self.pleasant_habit
-        )  # признак приятной привычки
-        award = dict(value).get(self.award)  # вознаграждение
-
-        if pleasant_habit and related_habit:
-            return "Нельзя выбрать одновременно признак приятной привычки и связаную привычку"
-
-        if related_habit and award:
-            return "Нельзя выбрать одновременно вознаграждение и связаную приятную привычку"
-
-        if not related_habit_instance:
-            if related_habit and not related_habit_instance:
-                return "вы выбрали не приятную привычу"
-
-        if pleasant_habit and award:
-            return "У приятной привычки не может быть вознаграждения"
-
-        if not pleasant_habit and not award and not related_habit:
-            return "У привычки должно быть вознаграждение либо связанная привычка"
-
-
-# habit_data = {
-#     "place": "home",
-#     "time_start": "09:00",
-#     "action": "exercise",
-#     "pleasant_habit": True,
-#     "related_habit": 1,
-#     "frequency": 1,
-#     "award": None,
-#     "time_to_complete": 60,
-#     "is_published": False,
-# }
-#
-# habit_data_2 = {
-#     "place": "home",
-#     "time_start": "09:00",
-#     "action": "exercise",
-#     "pleasant_habit": False,
-#     "related_habit": None,
-#     "frequency": 1,
-#     "award": "energy drink",
-#     "time_to_complete": 60,
-#     "is_published": False,
-# }
-# habit_orm_instance = HabitOrm(**habit_data)  # create an instance of HabitOrm
-# validator = HabitValidator("related_habit", "award", "pleasant_habit").__call__(
-#     habit_data, habit_data_2
-# )
-# validator(habit_data)
+    async def __call__(
+        self, habit_create: HabitCreate, relation_habit: HabitOrm | None, user: int
+    ):
+        # Проверка на одновременное заполнение reward и related_habit
+        if (habit_create.award is not None) and (
+            habit_create.related_habit is not None
+        ):
+            raise ValueError(
+                "Нельзя указывать одновременно вознаграждение и связанную привычку."
+            )
+        if (habit_create.pleasant_habit is True) and (
+            habit_create.related_habit is not None
+        ):
+            raise ValueError(
+                "Нельзя указывать одновременно что это привычка приятная и связанную привычку"
+            )
+        # Проверка на одновременное заполнение связканной привычки и вознаграждение
+        if (habit_create.pleasant_habit is True) and (habit_create.award is not None):
+            raise ValueError(
+                "Нельзя указывать одновременно что это приятная привычка и вознаграждение"
+            )
+        # Проверка связанной привычки
+        if (habit_create.related_habit is not None) and (relation_habit is not None):
+            if not relation_habit.pleasant_habit:
+                raise ValueError("Связанная привычка должна быть приятной.")
+            if relation_habit.user_id != user:
+                raise ValueError(
+                    "Связанная привычка должна принадлежать тому же пользователю."
+                )
