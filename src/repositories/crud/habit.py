1cf95f543e4d4e48eaa98539a3face8d8b14df18
user <mrlivedance@vk.com>
2024-08-11 23:39:23 +0300

    change crud
    Signed-off-by: user <mrlivedance@vk.com>

--- src/repositories/crud/habit.py
+++ src/repositories/crud/habit.py
@@ -9,10 +9,10 @@ from src.schemas.habit import HabitRead
 class HabitOrmCrud:
 
     @staticmethod
-    async def insert_habit(session: AsyncSession, data: schemas.HabitCreate, user):
+    async def insert_habit(session: AsyncSession, data: schemas.HabitCreate, user: int):
         """Создание привычки"""
         data = data.model_dump()
-        data["user_id"] = user.id  # user.id
+        data["user_id"] = user  # user.id
         habit = HabitOrm(**data)
         session.add(habit)
         await session.commit()
@@ -20,9 +20,14 @@ class HabitOrmCrud:
         return habit
 
     @staticmethod
-    async def get_habit_by_id(session: AsyncSession, habit_id: int):
+    async def get_habit_by_id(session: AsyncSession, habit_id: int, user_id: int):
         """Получение привычки по id"""
-        return await session.get(HabitOrm, habit_id)
+        result = await session.execute(
+            select(HabitOrm)
+            .where(HabitOrm.id == habit_id)
+            .where(HabitOrm.user_id == user_id)
+        )
+        return result.scalar_one_or_none()
 
     @staticmethod
     async def get_habit_by_user_id(session: AsyncSession, user_id: int):
@@ -40,10 +45,9 @@ class HabitOrmCrud:
     @staticmethod
     async def get_published_habits(session: AsyncSession):
         """Получение публичных привычек"""
-        result = await session.execute(
-            select(HabitOrm).where(HabitOrm.is_published == True)
-        )
-        return result.scalars().all()
+        stmt = select(HabitOrm).where(HabitOrm.is_published == True)
+        result: Result = await session.execute(stmt)
+        return list(result.scalars().all())
 
     @staticmethod
     async def update_habit(session: AsyncSession, habit: HabitOrm, habit_update: dict):
@@ -56,6 +60,13 @@ class HabitOrmCrud:
         return habit
 
     @staticmethod
-    async def delete_habit(session: AsyncSession, habit_id: int):
+    async def delete_habit(session: AsyncSession, habit_id: int, user_id: int):
         """Удаление привычки"""
-        pass
+        result = await session.get(HabitOrm, habit_id)
+        if result is not None:
+            if result.user_id == user_id:
+                await session.delete(result)
+                await session.commit()
+                return {"detail": "Success delete task"}
+            return {"detail": "User is not owner to task"}
+        return {"detail": f"There is no task with this number {habit_id} in the table"}
